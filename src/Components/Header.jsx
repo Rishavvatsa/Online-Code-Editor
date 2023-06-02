ff9698b1d0b871f75de483f977d329ef691dbb2a
Rishav Kumar <vatsarishav99@gmail.com>
2023-06-03 01:23:41 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,24 @@
+
+import { AppBar, Toolbar, styled } from "@mui/material"
+import logos from '../assets/codepen-ar21-removebg-preview.png'
+const Container = styled(AppBar)`
+  background:lightgreen;
+   height:9vh;
+    
+    
+`;
+
+const Header = () => {
+    
+
+    return (
+        <Container position="static" >
+            <Toolbar>
+                <img src={logos} alt="logo" style={{ width:180,height:100}} />
+                
+            </Toolbar>
+        </Container>
+    )
+}
+
+export default Header
